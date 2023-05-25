959a88db75549ae063c832b4dbe0552138339faa
Xenoxide <tayeemmachine@gmail.com>
2023-05-25 01:06:16 -0400

    Added some memory safety, tokenizing

--- t_obj.c
+++ t_obj.c
@@ -4,9 +4,25 @@
 
 #include "t_math.h"
 
-#define MAX_VERTICES 1000
-#define MAX_NORMALS 500
-#define MAX_FACES 500
+#define MAX_VERTICES 10000
+#define MAX_NORMALS 5000
+#define MAX_FACES 5000
+
+// It takes in a "1/2/3" and returns a [1, 2, 3].
+void t_splitFace(char* in, int* out) {
+    char copy[6];
+    char *token, *saveptr;
+
+    strcpy(copy, in);
+    token = strtok_r(copy, "/", &saveptr);
+
+    while (token != NULL) {
+        int number = atoi(token);
+        printf("%d\n", number);
+
+        token = strtok_r(NULL, "/", &saveptr);
+    }
+}
 
 // Takes in the filename of the obj file, and puts an
 // array of faces in the second argument t_Face* faces.
@@ -45,6 +61,8 @@ void t_decodeOBJ(char* filename, t_Face* faces) {
 
         // "v" Vertex
         if(!(strcmp(token_array[0], "v"))) {
+            // Haha... don't want to mess up the memory...
+            if (vertices_index == MAX_VERTICES) return;
             vertices[vertices_index++] = (t_Point) {
                 atof(token_array[1]),
                 atof(token_array[2]),
@@ -55,6 +73,7 @@ void t_decodeOBJ(char* filename, t_Face* faces) {
 
         // "vn" Normal
         if(!(strcmp(token_array[0], "vn"))) {
+            if (normals_index == MAX_NORMALS) return;
             normals[normals_index++] = (t_Point) {
                 atof(token_array[1]),
                 atof(token_array[2]),
@@ -66,9 +85,13 @@ void t_decodeOBJ(char* filename, t_Face* faces) {
         // "f" Face (3 sides)
         // 0 - normal
         // 1, 2, 3 - vertices
+        // Returns integers instead of t_Point, it will
+        // be processed later.
 
         if (!(strcmp(token_array[0], "f"))) {
-            face_references[faces_index][0] = ;
+            char* copy;
+            strcpy(copy, token_array[1]);
+            
         }
     }
     fclose(file);
